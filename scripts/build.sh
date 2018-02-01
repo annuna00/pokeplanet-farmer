c511fcb50652625e7584eba6ce4c27fdbff20305
annuna <annun@protonmail.com>
2018-02-01 15:27:05 -0300

    build tesseract from sources instead of installing from apt-get

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,8 @@
+#
 # opencv
+#
+sudo apt-get install libavcodec-dev libavformat-dev libgtk2.0-dev libswscale-dev -y
+
 if [ ! -d "opencv" ]; then
     git clone https://github.com/opencv/opencv
     cd opencv
@@ -6,15 +10,16 @@ if [ ! -d "opencv" ]; then
     cd release
     cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=/usr/local ..
     make
-    sudo make install
-    cd ../../
 else
     cd opencv/release
-    sudo make install
-    cd ../../
 fi
 
+sudo make install
+cd ../../
+
+#
 # subimage
+#
 git submodule update --recursive
 cd src/pokeplanet/lib/subimage
 mkdir build
@@ -23,5 +28,19 @@ cmake ../
 make
 cd ../../../../../
 
+#
 # tesseract
-sudo apt-get install tesseract-ocr -y
+#
+sudo apt-get install autoconf-archive automake g++ libleptonica-dev libtool -y
+
+if [ ! -d "tesseract" ]; then
+    git clone https://github.com/tesseract-ocr/tesseract.git
+fi
+
+cd tesseract
+./autogen.sh
+./configure
+make
+sudo make install
+sudo ldconfig
+cd ../
